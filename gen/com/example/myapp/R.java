16d7c9f09c07f887a3123fdaa9647e4b1c0b5407
Sresht <sresht@mit.edu>
2014-05-02 21:28:07 -0400

    added basic logic to calculate distance between two geopoints using the haversine algorithm

--- gen/com/example/myapp/R.java
+++ gen/com/example/myapp/R.java
@@ -1286,7 +1286,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f060019;
         public static final int action_menu_presenter=0x7f06001a;
         public static final int action_mode_close_button=0x7f060027;
-        public static final int action_settings=0x7f060040;
+        public static final int action_settings=0x7f060041;
         public static final int activity_chooser_view_content=0x7f060028;
         public static final int always=0x7f06000f;
         public static final int beginning=0x7f060014;
@@ -1333,6 +1333,7 @@ containing a value of this type.
         public static final int showTitle=0x7f06000a;
         public static final int split_action_bar=0x7f060021;
         public static final int submit_area=0x7f06003c;
+        public static final int submit_button=0x7f060040;
         public static final int tabMode=0x7f060006;
         public static final int terrain=0x7f060003;
         public static final int title=0x7f06002e;
--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.myapp;
 
+//import com.example.mapdemo.R;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends FragmentActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapClickListener, OnClickListener {
 
 	//main activity that runs the app
+	
+	private GoogleMap mMap;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+        // Try to obtain the map from the SupportMapFragment.
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        mMap.setOnMapClickListener(this);
+        
+    	Button doneButton = (Button) findViewById(R.id.submit_button);
+    	doneButton.setOnClickListener(this);
+
 
 //		if (savedInstanceState == null) {
 //			getSupportFragmentManager().beginTransaction()
@@ -30,34 +47,67 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private LatLng firstClick;
+	private LatLng lastClick;
+	@Override
+	public void onMapClick(LatLng position)
+	{
+		if(firstClick == null)
+		{
+			firstClick = position;
+		}
+		else
+		{
+			lastClick = position;
+		}		
+		mMap.addMarker(new MarkerOptions().position(position)
+				  );
+	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	public void onClick(View v) 
+	{
+		if(firstClick != null && lastClick != null)
+		{
+			PolylineOptions line = new PolylineOptions().add(
+					firstClick,
+					lastClick);
+			mMap.addPolyline(line);
+			System.out.println(findDistance(firstClick, lastClick));
 		}
-		return super.onOptionsItemSelected(item);
+		else
+		{
+			System.out.println("you need more points!");
+		}
 	}
-
+	
 	/**
-	 * A placeholder fragment containing a simple view.
+	 * calculates the distance between two points using the haversine formula
 	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+	private double findDistance(LatLng firstPoint, LatLng secondPoint)
+	{
+		final double radiusOfEarth = 6378;
+		double latitudeDistance = inRadians(firstPoint.latitude - secondPoint.latitude);
+		double longitudeDistance = inRadians(firstPoint.longitude - secondPoint.longitude);
+		double a = Math.pow((Math.sin(latitudeDistance / 2)) , 2) 
+				+ Math.cos(inRadians(firstPoint.latitude)) * Math.cos(inRadians(secondPoint.latitude))
+				  * Math.pow((Math.sin(longitudeDistance / 2)) , 2);
+		return 2 * radiusOfEarth * Math.atan2(Math.pow(a, 0.5), Math.pow(1 - a, 0.5));
 	}
+	
+	private double inRadians(Double degree)
+	{
+		return degree * Math.PI / 180.0;
+	}
+
+
+//	public void doCalculations()
+//	{
+//		
+//	}
+
+
+
 
 }
