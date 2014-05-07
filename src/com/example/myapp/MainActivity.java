6fa75d4930b4bb8736e687a8d97b1868f976a818
Sresht <sresht@mit.edu>
2014-05-07 16:06:30 -0400

    taking out gen and bin files

--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.myapp;
 
 //import com.example.mapdemo.R;
+import android.content.Context;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -23,9 +26,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends FragmentActivity 
-						  implements OnMapClickListener, OnClickListener,
+						  implements OnMapClickListener,
 						  GooglePlayServicesClient.ConnectionCallbacks,
-					        GooglePlayServicesClient.OnConnectionFailedListener{
+					        GooglePlayServicesClient.OnConnectionFailedListener,
+					        com.google.android.gms.location.LocationListener {
 
 	//main activity that runs the app
 	
@@ -35,11 +39,36 @@ public class MainActivity extends FragmentActivity
 	private LatLng lastClick;
 	private LocationClient mLocationClient;
 	private Location mCurrentLocation; 
+	private LocationRequest mLocationRequest;
+	private boolean mUpdatesRequested;
+	
+	 // Milliseconds per second
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    // Update frequency in seconds
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    // The fastest update frequency, in seconds
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+	
+    private SharedPreferences mPrefs;
+    private SharedPreferences.Editor mEditor;
+    
+    private TextView mLatLng;
+
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
+		
+        mLatLng = (TextView) findViewById(R.id.label_lat_lng);
+
 		
         // Try to obtain the map from the SupportMapFragment.
         mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
@@ -47,9 +76,54 @@ public class MainActivity extends FragmentActivity
         mMap.setOnMapClickListener(this);
         
     	Button doneButton = (Button) findViewById(R.id.submit_button);
-    	doneButton.setOnClickListener(this);
+    	doneButton.setOnClickListener(new View.OnClickListener() {
+    	    @Override
+    	    public void onClick(View v) 
+    	    {
+    			if(firstClick != null && lastClick != null)
+    			{
+    				PolylineOptions line = new PolylineOptions().add(
+    						firstClick,
+    						lastClick);
+    				mMap.addPolyline(line);
+    				Toast.makeText(v.getContext(), String.valueOf(findDistance(firstClick, lastClick)), Toast.LENGTH_SHORT).show();
+    			}
+    			else
+    			{
+    				Toast.makeText(v.getContext(), "you need more points!", Toast.LENGTH_SHORT).show();
+    			}    	    
+    		}
+    	});
+    	
+    	Button getLocationButton = (Button) findViewById(R.id.get_location_button);
+    	getLocationButton.setOnClickListener(new View.OnClickListener() 
+    	{
+    	    @Override
+    	    public void onClick(View v) 
+    	    {
+    	        // If Google Play Services is available
+    	        if (servicesConnected()) 
+    	        {
+    	            // Get the current location
+    	            mCurrentLocation = mLocationClient.getLastLocation();
+    	            mLatLng.setText(mCurrentLocation.toString());
+        	    	Toast.makeText(v.getContext(), mCurrentLocation.toString(), Toast.LENGTH_SHORT).show();
+    	        }
+    	    }
+    	});
+   	
+    	mLocationRequest = LocationRequest.create();
+    	
+    	mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    	mLocationRequest.setInterval(UPDATE_INTERVAL);
+    	mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
     	
         mLocationClient = new LocationClient(this, this, this);
+        
+        mPrefs = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+        mEditor = mPrefs.edit();
+
+        mUpdatesRequested = true;
 	}
 
     /*
@@ -62,11 +136,46 @@ public class MainActivity extends FragmentActivity
         mLocationClient.connect();
     }
 
+    @Override
+    protected void onPause() {
+        // Save the current setting for updates
+        mEditor.putBoolean("KEY_UPDATES_ON", mUpdatesRequested);
+        mEditor.commit();
+        super.onPause();
+    }
+    
+    @Override
+    protected void onResume() {
+        /*
+         * Get any previous setting for location updates
+         * Gets "false" if an error occurs
+         */
+        if (mPrefs.contains("KEY_UPDATES_ON")) {
+            mUpdatesRequested =
+                    mPrefs.getBoolean("KEY_UPDATES_ON", false);
+
+        // Otherwise, turn off location updates
+        } else {
+            mEditor.putBoolean("KEY_UPDATES_ON", false);
+            mEditor.commit();
+        }
+        super.onResume();
+    }
+    
     /*
      * Called when the Activity is no longer visible.
      */
     @Override
     protected void onStop() {
+    	
+    	if (mLocationClient.isConnected()) {
+            /*
+             * Remove location updates for a listener.
+             * The current Activity is the listener, so
+             * the argument is "this".
+             */
+            mLocationClient.removeLocationUpdates(this);
+        }
         // Disconnecting the client invalidates it.
         mLocationClient.disconnect();
         super.onStop();
@@ -96,28 +205,24 @@ public class MainActivity extends FragmentActivity
 				  );
 	}
 
-	@Override
-	public void onClick(View v) 
-	{
-		if(firstClick != null && lastClick != null)
-		{
-			PolylineOptions line = new PolylineOptions().add(
-					firstClick,
-					lastClick);
-			mMap.addPolyline(line);
-			System.out.println(findDistance(firstClick, lastClick));
-		}
-		else
-		{
-			System.out.println("you need more points!");
-		}
-	}
+//	@Override
+//	public void onClick(View v) 
+//	{
+
+//	}
 	
     @Override
     public void onConnected(Bundle dataBundle) {
         // Display the connection status
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-        getLocation();
+        if (mUpdatesRequested) {
+            startPeriodicUpdates();
+        }
+    }
+        
+    private void startPeriodicUpdates()
+    {
+    	mLocationClient.requestLocationUpdates(mLocationRequest, this);
     }
 
     /*
@@ -166,7 +271,20 @@ public class MainActivity extends FragmentActivity
   		  	GooglePlayServicesUtil.getErrorDialog(errorCode, this, 0).show();
         }
     }
-	
+    
+    // Define the callback method that receives location updates
+    //TODO WHY ISN'T THIS WORKING
+    @Override
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+//        mLatLng.setText(msg);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    
+    
 	private boolean servicesConnected() {
         // Check that Google Play services is available
 		int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
@@ -176,25 +294,6 @@ public class MainActivity extends FragmentActivity
 		  return false;
 		}
 		return true;
-    }
-	
-    /**
-     * Invoked by the "Get Location" button.
-     *
-     * Calls getLastLocation() to get the current location
-     *
-     * @param v The view object associated with this method, in this case a Button.
-     */
-    public void getLocation() {
-
-        // If Google Play Services is available
-        if (servicesConnected()) {
-
-            // Get the current location
-            mCurrentLocation = mLocationClient.getLastLocation();
-            System.out.println("current location");
-            System.out.println(mCurrentLocation.toString());
-        }
     }
 	
 	
